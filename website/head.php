0c76e6da0853da03b6653aec928042aac452fc3a
zteeed <zteeed@minet.net>
2018-10-04 15:41:17 +0200

    fix local path

--- website/head.php
+++ website/head.php
@@ -3,7 +3,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-<link rel="stylesheet" href="/assets/css/main.css" />
+<link rel="stylesheet" href="assets/css/main.css" />
 <link rel="icon" href="/images/favicon.ico" />
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
 <link class="codestyle" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/monokai-sublime.min.css">
