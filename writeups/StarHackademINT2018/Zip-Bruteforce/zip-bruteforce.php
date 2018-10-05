923bd75903325fe49ed0a3d96fd9a453975f2935
zteeed <zteeed@minet.net>
2018-10-05 00:00:17 +0000

    fix path

--- writeups/StarHackademINT2018/Zip-Bruteforce/zip-bruteforce.php
+++ writeups/StarHackademINT2018/Zip-Bruteforce/zip-bruteforce.php
@@ -1,7 +1,7 @@
 <h4 style="margin-top: 30px">Objectif</h4>
-Il s'agit dans ce challenge de retrouver le mot de passe du fichier <a href="WRITEUPS/StarHackademINT2018/Zip-Bruteforce/flag.zip">flag.zip</a> afin de retrouver la liste de course pour notre animal de compagnie affamé Jean Didier<br />
+Il s'agit dans ce challenge de retrouver le mot de passe du fichier <a href="/writeups/StarHackademINT2018/Zip-Bruteforce/flag.zip">flag.zip</a> afin de retrouver la liste de course pour notre animal de compagnie affamé Jean Didier<br />
 <p style="margin-top: 30px; text-align: center">
-  <img src="WRITEUPS/StarHackademINT2018/Zip-Bruteforce/koala.jpg" width="80%" />
+  <img src="/writeups/StarHackademINT2018/Zip-Bruteforce/koala.jpg" width="80%" />
 </p>
 <br />
 <h4>Méthode</h4>
@@ -23,5 +23,5 @@ La commande renverra au bout d'un certain temps (patience...):<br/>
 PASSWORD FOUND!!!!: pw = koala
 </code></pre>
 </p>
-A partir de là, il ne reste plus qu'à saisir le mot de passe pour extraire le fichier <a href="WRITEUPS/StarHackademINT2018/Zip-Bruteforce/flag.txt">flag.txt</a> qui contient le flag recherché !
+A partir de là, il ne reste plus qu'à saisir le mot de passe pour extraire le fichier <a href="/writeups/StarHackademINT2018/Zip-Bruteforce/flag.txt">flag.txt</a> qui contient le flag recherché !
 (mais aucune trace de liste de course... dommage)
