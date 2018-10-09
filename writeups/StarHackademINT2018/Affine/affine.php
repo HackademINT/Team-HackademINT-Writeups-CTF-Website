731bda721073d33e8d59cfe4cb12cab0aaa3c3f6
LemonIceStuff <arthur.byr01@yahoo.fr>
2018-10-09 18:43:17 +0200

    Add affine writeup

---
+++ writeups/StarHackademINT2018/Affine/affine.php
@@ -0,0 +1,28 @@
+<h4 style="margin-top: 30px">Objectif</h4>
+
+Dans ce challenge, on doit déchiffrer un texte chiffré à partir de ce script python : <a href="/writeups/StarHackademINT2018/Zip-Bruteforce/flag.zip">flag.zip</a> fourni sur le site du challenge. <br />
+<p style="margin-top: 30px; text-align: center">
+  <img src="/writeups/StarHackademINT2018/Zip-Bruteforce/koala.jpg" width="80%" />
+</p>
+<br />
+<h4>Méthode</h4>
+
+Comme suggéré par l'énoncé, on va utiliser ici fcrackzip en mode bruteforce pour trouver le
+mot de passe qu'on sait de longueur strictement inférieure à 6.
+<br/>
+
+Il suffit alors de consulter la page de manuel de fcrackzip afin de connaitre les arguments qui nous sont utiles ici: <br/>
+-b pour bruteforce <br/>
+-l 1-5 pour indiquer qu'on cherche un mot de passe de longueur comprise entre 1 et 5 <br/>
+-u pour que fcrackzip teste pour nous toutes les possibilités et nous renvoie celle qui marche. <br/>
+
+La commande renverra au bout d'un certain temps (patience...):<br/>
+<p style="margin-top: 30px">
+<pre><code>fcrackzip -b -l 1-5 -u flag.zip
+
+
+PASSWORD FOUND!!!!: pw = koala
+</code></pre>
+</p>
+A partir de là, il ne reste plus qu'à saisir le mot de passe pour extraire le fichier <a href="/writeups/StarHackademINT2018/Zip-Bruteforce/flag.txt">flag.txt</a> qui contient le flag recherché !
+(mais aucune trace de liste de course... dommage)
